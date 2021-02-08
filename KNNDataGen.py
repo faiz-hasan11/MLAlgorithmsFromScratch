285bbc2dda24e1ef4137791c5e3f2edd7fb681f8
Syed Faiz Hasan <61085713+faiz-hasan11@users.noreply.github.com>
2021-02-08 12:41:17 +0530

    Update KNNDataGen.py

--- KNNDataGen.py
+++ KNNDataGen.py
@@ -15,9 +15,22 @@ X_train, X_test, Y_train, Y_test = train_test_split(
 # plt.scatter(X[:, 0], X[:, 1], c=Y, cmap='viridis', edgecolors='k', s=20)
 # plt.show()
 
-clf = KNN(k=3)
-clf.fit(X_train, Y_train)
-predictions = clf.predict(X_test)
 
-acc = np.sum(predictions == Y_test) / len(Y_test)
-print(acc)
+def best_params(acc_max, k_best):
+    for k_val in range(3, 11, 2):
+        clf = KNN(k=k_val)
+        clf.fit(X_train, Y_train)
+        predictions = clf.predict(X_test)
+        acc = np.sum(predictions == Y_test) / len(Y_test)
+        if acc > acc_max:
+            acc_max = acc
+            k_best = k_val
+    return (acc_max, k_best)
+
+
+acc_max = 0
+k_best = 3
+acc_max, k_best = best_params(acc_max, k_best)
+
+print("Best Accuracy:", acc_max)
+print("Best Value of K:", k_best)
